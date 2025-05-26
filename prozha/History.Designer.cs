f7b7e2950135002cf313ea4561df8c301399b192
mustafa <mustafa.salah.2131@gmail.com>
2025-05-26 14:06:14 +0300

    Update History form layout and refactor database connection to use SQLite

--- prozha/History.Designer.cs
+++ prozha/History.Designer.cs
@@ -45,9 +45,9 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Lucida Sans Unicode", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label1.Location = new System.Drawing.Point(65, 182);
+            this.label1.Location = new System.Drawing.Point(73, 228);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(74, 34);
+            this.label1.Size = new System.Drawing.Size(87, 40);
             this.label1.TabIndex = 0;
             this.label1.Text = "Easy";
             // 
@@ -56,9 +56,9 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Cooper Black", 34F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label2.Location = new System.Drawing.Point(88, 23);
+            this.label2.Location = new System.Drawing.Point(99, 29);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(246, 65);
+            this.label2.Size = new System.Drawing.Size(294, 78);
             this.label2.TabIndex = 1;
             this.label2.Text = "History";
             // 
@@ -67,9 +67,9 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Lucida Sans Unicode", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label3.Location = new System.Drawing.Point(45, 254);
+            this.label3.Location = new System.Drawing.Point(51, 318);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(113, 34);
+            this.label3.Size = new System.Drawing.Size(134, 40);
             this.label3.TabIndex = 2;
             this.label3.Text = "Normal";
             // 
@@ -78,9 +78,9 @@
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Lucida Sans Unicode", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label4.Location = new System.Drawing.Point(65, 318);
+            this.label4.Location = new System.Drawing.Point(73, 398);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(80, 34);
+            this.label4.Size = new System.Drawing.Size(94, 40);
             this.label4.TabIndex = 3;
             this.label4.Text = "Hard";
             // 
@@ -89,9 +89,9 @@
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Lucida Sans Unicode", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label5.Location = new System.Drawing.Point(330, 182);
+            this.label5.Location = new System.Drawing.Point(371, 228);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(34, 34);
+            this.label5.Size = new System.Drawing.Size(39, 40);
             this.label5.TabIndex = 4;
             this.label5.Text = "0";
             // 
@@ -100,9 +100,9 @@
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Lucida Sans Unicode", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label6.Location = new System.Drawing.Point(330, 254);
+            this.label6.Location = new System.Drawing.Point(371, 318);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(34, 34);
+            this.label6.Size = new System.Drawing.Size(39, 40);
             this.label6.TabIndex = 5;
             this.label6.Text = "0";
             // 
@@ -111,9 +111,9 @@
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Lucida Sans Unicode", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label7.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label7.Location = new System.Drawing.Point(330, 318);
+            this.label7.Location = new System.Drawing.Point(371, 398);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(34, 34);
+            this.label7.Size = new System.Drawing.Size(39, 40);
             this.label7.TabIndex = 6;
             this.label7.Text = "0";
             // 
@@ -122,9 +122,9 @@
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label8.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label8.Location = new System.Drawing.Point(18, 124);
+            this.label8.Location = new System.Drawing.Point(20, 155);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(133, 32);
+            this.label8.Size = new System.Drawing.Size(151, 38);
             this.label8.TabIndex = 7;
             this.label8.Text = "Difficulty";
             // 
@@ -133,9 +133,9 @@
             this.label9.AutoSize = true;
             this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold);
             this.label9.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label9.Location = new System.Drawing.Point(218, 125);
+            this.label9.Location = new System.Drawing.Point(245, 156);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(201, 32);
+            this.label9.Size = new System.Drawing.Size(235, 38);
             this.label9.TabIndex = 8;
             this.label9.Text = "Correct words";
             // 
@@ -143,9 +143,10 @@
             // 
             this.button1.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold);
             this.button1.ForeColor = System.Drawing.Color.Brown;
-            this.button1.Location = new System.Drawing.Point(26, 370);
+            this.button1.Location = new System.Drawing.Point(29, 462);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(440, 43);
+            this.button1.Size = new System.Drawing.Size(495, 54);
             this.button1.TabIndex = 9;
             this.button1.Text = "Close";
             this.button1.UseVisualStyleBackColor = true;
@@ -153,10 +154,10 @@
             // 
             // History
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Firebrick;
-            this.ClientSize = new System.Drawing.Size(524, 467);
+            this.ClientSize = new System.Drawing.Size(550, 584);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.label8);
@@ -169,6 +170,7 @@
             this.Controls.Add(this.label1);
             this.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "History";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "History";
--- prozha/History.cs
+++ prozha/History.cs
@@ -1,14 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Data.SqlClient;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Collections.Generic;  
 using System.Windows.Forms;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement.ProgressBar;
+using System.Data.SQLite;
 
 namespace prozha
 {
@@ -21,38 +14,38 @@ namespace prozha
          
         private void History_Load(object sender, EventArgs e)
         {
-
-            SqlConnection connection = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=\"C:\\Users\\musta\\OneDrive\\Desktop\\test typing\\prozha\\words.mdf\";Integrated Security=True;Connect Timeout=30");
-            connection.Open();
-             
-
-            var modes = new[] { "easy", "normal", "hard" };
-            List<string> results = new List<string>();
-
-            foreach (var mode in modes)
+            using (SQLiteConnection connection = new SQLiteConnection("Data Source=typing_test.db"))
             {
-                string query =  "SELECT result FROM History WHERE difficulty = '"+mode+"'";
-                SqlCommand command = new SqlCommand(query, connection); 
-                SqlDataReader reader = command.ExecuteReader();
+                connection.Open();
 
-                if (reader.Read())
+                var modes = new[] { "Easy", "Normal", "Hard" };
+                List<string> results = new List<string>();
+
+                foreach (var mode in modes)
                 {
-                    string result = reader["result"].ToString();
-                    results.Add(string.IsNullOrEmpty(result) ? "0" : result);
-                }
-                else
-                {
-                    results.Add("0");
+                    string query = "SELECT result FROM History WHERE difficulty = @mode";
+                    using (SQLiteCommand command = new SQLiteCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@mode", mode);
+                        using (SQLiteDataReader reader = command.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                string result = reader["result"].ToString();
+                                results.Add(string.IsNullOrEmpty(result) ? "0" : result);
+                            }
+                            else
+                            {
+                                results.Add("0");
+                            }
+                        }
+                    }
                 }
 
-                reader.Close();
+                label5.Text = results[0];
+                label6.Text = results[1];
+                label7.Text = results[2];
             }
-
-            label5.Text = results[0];
-            label6.Text = results[1];
-            label7.Text = results[2];
-
-            connection.Close();
         }
 
         private void button1_Click(object sender, EventArgs e)
