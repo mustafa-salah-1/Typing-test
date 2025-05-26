51b7dbc34f810cddf470fc0dd7fea2ed1f475016
mustafa <mustafa.salah.2131@gmail.com>
2025-05-26 14:05:30 +0300

    change name form1 to Main

--- prozha/Main.Designer.cs
+++ prozha/Main.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace prozha
 {
-    partial class Form1
+    partial class Main
     {
         /// <summary>
         /// Required designer variable.
@@ -29,7 +29,7 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
             this.label1 = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.textBox1 = new System.Windows.Forms.TextBox();
@@ -56,9 +56,9 @@
             this.label1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(234)))), ((int)(((byte)(234)))), ((int)(((byte)(0)))));
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.label1.Location = new System.Drawing.Point(219, 46);
+            this.label1.Location = new System.Drawing.Point(246, 58);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(179, 95);
+            this.label1.Size = new System.Drawing.Size(214, 113);
             this.label1.TabIndex = 0;
             this.label1.Text = "text";
             // 
@@ -72,12 +72,12 @@
             // 
             this.textBox1.Enabled = false;
             this.textBox1.Font = new System.Drawing.Font("Tahoma", 26F);
-            this.textBox1.Location = new System.Drawing.Point(82, 178);
+            this.textBox1.Location = new System.Drawing.Point(92, 222);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(605, 60);
+            this.textBox1.Size = new System.Drawing.Size(680, 70);
             this.textBox1.TabIndex = 4;
             this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
             // 
             // label2
@@ -85,9 +85,9 @@
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label2.Location = new System.Drawing.Point(112, 299);
+            this.label2.Location = new System.Drawing.Point(126, 374);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(147, 42);
+            this.label2.Size = new System.Drawing.Size(173, 52);
             this.label2.TabIndex = 6;
             this.label2.Text = "Correct";
             // 
@@ -106,7 +106,8 @@
             this.circularProgressBar1.InnerColor = System.Drawing.Color.Brown;
             this.circularProgressBar1.InnerMargin = 2;
             this.circularProgressBar1.InnerWidth = -1;
-            this.circularProgressBar1.Location = new System.Drawing.Point(446, 386);
+            this.circularProgressBar1.Location = new System.Drawing.Point(502, 482);
+            this.circularProgressBar1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.circularProgressBar1.MarqueeAnimationSpeed = 2000;
             this.circularProgressBar1.Maximum = 5;
             this.circularProgressBar1.Name = "circularProgressBar1";
@@ -116,7 +117,7 @@
             this.circularProgressBar1.ProgressColor = System.Drawing.Color.IndianRed;
             this.circularProgressBar1.ProgressWidth = 20;
             this.circularProgressBar1.SecondaryFont = new System.Drawing.Font("Tahoma", 36F);
-            this.circularProgressBar1.Size = new System.Drawing.Size(242, 176);
+            this.circularProgressBar1.Size = new System.Drawing.Size(272, 220);
             this.circularProgressBar1.StartAngle = 270;
             this.circularProgressBar1.Step = 60;
             this.circularProgressBar1.SubscriptColor = System.Drawing.Color.FromArgb(((int)(((byte)(8)))), ((int)(((byte)(131)))), ((int)(((byte)(149)))));
@@ -135,9 +136,9 @@
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 22F, System.Drawing.FontStyle.Bold);
             this.label7.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.label7.Location = new System.Drawing.Point(505, 299);
+            this.label7.Location = new System.Drawing.Point(568, 374);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(104, 42);
+            this.label7.Size = new System.Drawing.Size(124, 52);
             this.label7.TabIndex = 10;
             this.label7.Text = "Time";
             // 
@@ -151,7 +152,8 @@
             this.circularProgressBar2.InnerColor = System.Drawing.Color.Brown;
             this.circularProgressBar2.InnerMargin = 2;
             this.circularProgressBar2.InnerWidth = -1;
-            this.circularProgressBar2.Location = new System.Drawing.Point(82, 386);
+            this.circularProgressBar2.Location = new System.Drawing.Point(92, 482);
+            this.circularProgressBar2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.circularProgressBar2.MarqueeAnimationSpeed = 2000;
             this.circularProgressBar2.Name = "circularProgressBar2";
             this.circularProgressBar2.OuterColor = System.Drawing.Color.Brown;
@@ -160,7 +162,7 @@
             this.circularProgressBar2.ProgressColor = System.Drawing.Color.IndianRed;
             this.circularProgressBar2.ProgressWidth = 20;
             this.circularProgressBar2.SecondaryFont = new System.Drawing.Font("Tahoma", 36F);
-            this.circularProgressBar2.Size = new System.Drawing.Size(242, 176);
+            this.circularProgressBar2.Size = new System.Drawing.Size(272, 220);
             this.circularProgressBar2.StartAngle = 270;
             this.circularProgressBar2.SubscriptColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
             this.circularProgressBar2.SubscriptMargin = new System.Windows.Forms.Padding(10, -35, 0, 0);
@@ -177,6 +179,7 @@
             // 
             this.menuStrip1.BackColor = System.Drawing.Color.White;
             this.menuStrip1.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.menuStrip1.GripMargin = new System.Windows.Forms.Padding(2, 2, 0, 2);
             this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.fileToolStripMenuItem,
@@ -184,8 +187,8 @@
             this.aboutToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Padding = new System.Windows.Forms.Padding(7, 2, 0, 2);
-            this.menuStrip1.Size = new System.Drawing.Size(779, 31);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(8, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(876, 36);
             this.menuStrip1.TabIndex = 21;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -195,20 +198,20 @@
             this.historyToolStripMenuItem,
             this.exitToolStripMenuItem});
             this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-            this.fileToolStripMenuItem.Size = new System.Drawing.Size(49, 27);
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(58, 32);
             this.fileToolStripMenuItem.Text = "File";
             // 
             // historyToolStripMenuItem
             // 
             this.historyToolStripMenuItem.Name = "historyToolStripMenuItem";
-            this.historyToolStripMenuItem.Size = new System.Drawing.Size(147, 28);
+            this.historyToolStripMenuItem.Size = new System.Drawing.Size(177, 36);
             this.historyToolStripMenuItem.Text = "History";
             this.historyToolStripMenuItem.Click += new System.EventHandler(this.historyToolStripMenuItem_Click);
             // 
             // exitToolStripMenuItem
             // 
             this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
-            this.exitToolStripMenuItem.Size = new System.Drawing.Size(147, 28);
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(177, 36);
             this.exitToolStripMenuItem.Text = "Exit";
             this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
             // 
@@ -219,27 +222,27 @@
             this.normalToolStripMenuItem,
             this.hardToolStripMenuItem});
             this.defficultyToolStripMenuItem.Name = "defficultyToolStripMenuItem";
-            this.defficultyToolStripMenuItem.Size = new System.Drawing.Size(95, 27);
+            this.defficultyToolStripMenuItem.Size = new System.Drawing.Size(111, 32);
             this.defficultyToolStripMenuItem.Text = "Defficulty";
             // 
             // easyToolStripMenuItem
             // 
             this.easyToolStripMenuItem.Name = "easyToolStripMenuItem";
-            this.easyToolStripMenuItem.Size = new System.Drawing.Size(224, 28);
+            this.easyToolStripMenuItem.Size = new System.Drawing.Size(180, 36);
             this.easyToolStripMenuItem.Text = "Easy";
             this.easyToolStripMenuItem.Click += new System.EventHandler(this.easyToolStripMenuItem_Click);
             // 
             // normalToolStripMenuItem
             // 
             this.normalToolStripMenuItem.Name = "normalToolStripMenuItem";
-            this.normalToolStripMenuItem.Size = new System.Drawing.Size(224, 28);
+            this.normalToolStripMenuItem.Size = new System.Drawing.Size(180, 36);
             this.normalToolStripMenuItem.Text = "Normal";
             this.normalToolStripMenuItem.Click += new System.EventHandler(this.normalToolStripMenuItem_Click);
             // 
             // hardToolStripMenuItem
             // 
             this.hardToolStripMenuItem.Name = "hardToolStripMenuItem";
-            this.hardToolStripMenuItem.Size = new System.Drawing.Size(224, 28);
+            this.hardToolStripMenuItem.Size = new System.Drawing.Size(180, 36);
             this.hardToolStripMenuItem.Text = "Hard";
             this.hardToolStripMenuItem.Click += new System.EventHandler(this.hardToolStripMenuItem_Click);
             // 
@@ -247,16 +250,16 @@
             // 
             this.aboutToolStripMenuItem.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
-            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(71, 27);
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(83, 32);
             this.aboutToolStripMenuItem.Text = "About";
             this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Brown;
-            this.ClientSize = new System.Drawing.Size(779, 609);
+            this.ClientSize = new System.Drawing.Size(876, 761);
             this.Controls.Add(this.circularProgressBar2);
             this.Controls.Add(this.circularProgressBar1);
             this.Controls.Add(this.label7);
@@ -265,6 +268,7 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
             this.ShowIcon = false;
             this.ShowInTaskbar = false;
---
+++ prozha/Main.cs
@@ -0,0 +1,291 @@
+﻿using System; 
+using System.Data; 
+using System.Data.SQLite;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace prozha
+{
+    public partial class Main : Form  
+    {
+        public Main()
+        {
+            InitializeComponent(); 
+            Database db = new Database();
+            db.CreateTable("letter_four");
+            db.CreateTable("letter_five");
+            db.CreateTable("letter_six");
+            db.CreateHistoryTable();
+            db.AddWordsToTable("letter_four", db.wordsFour);
+            db.AddWordsToTable("letter_five", db.wordsFive);
+            db.AddWordsToTable("letter_six", db.wordsSix);
+        }
+
+        public static string connStr = "Data Source=typing_test.db;";
+        SQLiteConnection connection = new SQLiteConnection(connStr);
+         
+        private static int correct = 0;  
+        private static int second = 5;
+        Random rnd = new Random();
+        public static string difficult;
+        public static string mode;
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            second--; 
+             if(correct == 100)
+             {
+                timer2.Enabled = false;
+                textBox1.Enabled = false;
+
+                MessageBox.Show(correct.ToString() + " words. Best typing for this difficulty.", "Result");
+
+                connection.Open();
+                string query = "SELECT difficulty FROM History WHERE difficulty = @mode";
+                SQLiteCommand check = new SQLiteCommand(query, connection);
+                check.Parameters.AddWithValue("@mode", mode);
+
+                DataTable dt = new DataTable();
+                SQLiteDataAdapter da = new SQLiteDataAdapter(check);
+                da.Fill(dt);
+
+                if (dt.Rows.Count > 0)
+                {
+                    // Update existing row
+                    string updateQuery = "UPDATE History SET result = @correct WHERE difficulty = @mode";
+                    SQLiteCommand update = new SQLiteCommand(updateQuery, connection);
+                    update.Parameters.AddWithValue("@correct", correct);
+                    update.Parameters.AddWithValue("@mode", mode);
+                    update.ExecuteNonQuery();
+                }
+                else
+                {
+                    // Insert new row
+                    string insertQuery = "INSERT INTO History (difficulty, result) VALUES (@mode, @correct)";
+                    SQLiteCommand insert = new SQLiteCommand(insertQuery, connection);
+                    insert.Parameters.AddWithValue("@mode", mode);
+                    insert.Parameters.AddWithValue("@correct", correct);
+                    insert.ExecuteNonQuery();
+                }
+
+                connection.Close();
+             }
+             else if(second == 0)
+             {
+                timer2.Enabled = false;
+                textBox1.Enabled = false;
+                if (correct > 20)
+                {
+                    MessageBox.Show(correct.ToString() + " words. you are Legend type.", "Result");
+                }
+                else if (correct > 15)
+                {
+                    MessageBox.Show(correct.ToString() + " words. Very Good type.", "Result");
+                }
+                else if (correct > 5)
+                {
+                    MessageBox.Show(correct.ToString() + " words. Good type.", "Result");
+                }
+                else
+                {
+                    MessageBox.Show(correct.ToString() + " words. Bad type.", "Result");
+                }
+                connection.Open();
+
+                string checkQuery = "SELECT difficulty FROM History WHERE difficulty = @mode";
+                using (SQLiteCommand checkCmd = new SQLiteCommand(checkQuery, connection))
+                {
+                    checkCmd.Parameters.AddWithValue("@mode", mode);
+
+                    DataTable dt = new DataTable();
+                    using (SQLiteDataAdapter da = new SQLiteDataAdapter(checkCmd))
+                    {
+                        da.Fill(dt);
+                    }
+
+                    if (dt.Rows.Count > 0)
+                    { 
+                        string resultQuery = "SELECT result FROM History WHERE difficulty = @mode";
+                        using (SQLiteCommand resultCmd = new SQLiteCommand(resultQuery, connection))
+                        {
+                            resultCmd.Parameters.AddWithValue("@mode", mode);
+
+                            using (SQLiteDataReader reader = resultCmd.ExecuteReader())
+                            {
+                                if (reader.Read())
+                                {
+                                    int existingResult = int.Parse(reader["result"].ToString());
+                                     
+                                    if (correct > existingResult)
+                                    {
+                                        string updateQuery = "UPDATE History SET result = @correct WHERE difficulty = @mode";
+                                        using (SQLiteCommand updateCmd = new SQLiteCommand(updateQuery, connection))
+                                        {
+                                            updateCmd.Parameters.AddWithValue("@correct", correct);
+                                            updateCmd.Parameters.AddWithValue("@mode", mode);
+                                            updateCmd.ExecuteNonQuery();
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                    else
+                    { 
+                        string insertQuery = "INSERT INTO History (difficulty, result) VALUES (@mode, @correct)";
+                        using (SQLiteCommand insertCmd = new SQLiteCommand(insertQuery, connection))
+                        {
+                            insertCmd.Parameters.AddWithValue("@mode", mode);
+                            insertCmd.Parameters.AddWithValue("@correct", correct);
+                            insertCmd.ExecuteNonQuery();
+                        }
+                    }
+                }
+                connection.Close();
+
+            }
+        }
+        Boolean make = false;
+        private void timer1_Tick(object sender, EventArgs e)
+        { 
+            if (textBox1.Text.Length > 0 && make == false)
+            {
+                timer2.Enabled = true;
+                make = true;
+            }
+            circularProgressBar2.Value = correct;
+            circularProgressBar2.Text = correct.ToString();
+            circularProgressBar1.Value = second;
+            circularProgressBar1.Text = second.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {   
+            textBox1.Focus();
+            label1.Text = ""; 
+        } 
+         
+        private void start()
+        { 
+            string[] allowedTables = { "Easy", "Normal", "Hard" }; 
+            if (!allowedTables.Contains(mode))
+            {
+                MessageBox.Show("Invalid difficulty level.");
+                return;
+            }
+
+            int randomId = rnd.Next(1, 101);  
+
+            using (SQLiteConnection connection = new SQLiteConnection("Data Source=typing_test.db;"))
+            {
+                connection.Open();
+
+                string query = $"SELECT word FROM {difficult} WHERE id = {randomId}";
+                using (SQLiteCommand letter = new SQLiteCommand(query, connection))
+                using (SQLiteDataReader read = letter.ExecuteReader())
+                {
+                    if (read.Read())
+                    {
+                        label1.Text = read["word"].ToString();
+                    }
+                    else
+                    {
+                        label1.Text = "(No word found)";
+                    }
+                }
+
+                connection.Close();
+            }
+
+            make = false;
+            newgame();
+        }
+        private void newgame()
+        {
+            try
+            {
+                second = 5; 
+                correct = 0; 
+                textBox1.Text = "";
+                textBox1.Enabled = true;
+                textBox1.Focus();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("error" + ex);
+            }
+        }
+
+        private void easyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            difficult = "letter_four";
+            mode = "Easy";
+            start();
+        }
+        private void normalToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            difficult = "letter_five";
+            mode = "Normal";
+            start();
+        } 
+        private void hardToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            difficult = "letter_six";
+            mode = "Hard";
+            start();
+        } 
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                e.SuppressKeyPress = true; 
+
+                if (label1.Text == textBox1.Text)
+                {
+                    correct++;
+                    second = 5; 
+                    int randomId = rnd.Next(1, 101);
+                    string sql = $"SELECT word FROM {difficult} WHERE id = @id";
+
+                    using (SQLiteConnection connection = new SQLiteConnection("Data Source=typing_test.db;"))
+                    {
+                        connection.Open();
+                        using (SQLiteCommand letter = new SQLiteCommand(sql, connection))
+                        {
+                            letter.Parameters.AddWithValue("@id", randomId);
+                            using (SQLiteDataReader read = letter.ExecuteReader())
+                            {
+                                if (read.Read())
+                                {
+                                    label1.Text = read["word"].ToString();
+                                }
+                                else
+                                {
+                                    label1.Text = "(No word found)";
+                                }
+                            }
+                        }
+                    }
+                }
+
+                textBox1.Clear();
+            }
+        }  
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Developed by Mustafa Salah", "Typing test");
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close(); 
+        }
+
+        private void historyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            History asa = new History();
+            asa.Show();
+        }
+         
+    }
+}
